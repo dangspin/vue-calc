cc35f5a21db11e981112bb4688ce66f317a3baf4
dangspin <dangxiaoqian@gmail.com>
2020-05-01 18:27:33 +0500

    一个初步能用的vue计算器

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 var app = new Vue({
   el: "#calculator",
   data: {
-    message: ""
+    message: "",
+    isFinished: false
   },
   methods:{
     clear() {
       this.message = "";
+    },
+    show(ele) {
+      if (typeof(ele) === "string") {
+        this.message = this.message + ele;
+      } else {
+        this.message = this.message + ele.toString();
+      }
+    },
+    equal() {
+      this.message = eval(this.message);
+    },
+    Sqrt() {
+      try {
+        var res = parseFloat(this.message);
+        res = Math.sqrt(res);
+      } catch(err) {
+        this.message = err;
+      }
+
+      this.message = res.toString();
     }
   }
 })
